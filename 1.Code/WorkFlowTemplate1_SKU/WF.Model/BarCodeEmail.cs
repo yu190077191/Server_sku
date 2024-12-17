639a1d37994a2161d981bf927a49062d31f61a7c
small-xbd <18842666760@163.com>
2024-12-17 16:08:13 +0800

    提交实体类

---
+++ 1.Code/WorkFlowTemplate1_SKU/WF.Model/BarCodeEmail.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace WF.Model
+{
+    [Serializable]
+    [XmlType("BarCodeEmail")]
+    public class BarCodeEmail
+    {
+        [XmlElement("Id")]
+        public int Id { get; set; }
+
+        [XmlElement("Type")]
+        public string Type { get; set; }
+
+        [XmlElement("StepNumber")]
+        public int StepNumber { get; set; }
+
+        [XmlElement("BuName")]
+        public string BuName { get; set; }
+
+        [XmlElement("EventContainerName")]
+        public string EventContainerName { get; set; }
+
+        [XmlElement("Category_Classification")]
+        public string Category_Classification { get; set; }
+
+        [XmlElement("mail")]
+        public string mail { get; set; }
+
+        [XmlElement("DisplayName")]
+        public string DisplayName { get; set; }
+
+        [XmlElement("Checked")]
+        public bool Checked { get; set; }
+    }
+}
